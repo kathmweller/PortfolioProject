a736d50f63649cd1474fc5f88250c43baed723b7
kathmweller <kathmweller@gmail.com>
2018-10-12 20:03:49 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+
+gulp.task("default", function() {
+  // code for your default task goes here
+  // run 'cd C:\Users\bradl\Documents\UdacityProjects\PortfolioProject' in terminal to get to "root" of your project
+
+  console.log('Hello Kathy');
+});
